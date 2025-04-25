5cbc25c17ad3cf8f1d0be068fdc6f992eb897b4d
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-04-25 16:38:39 +0900

    sessionの状態に応じてボタンを切り替えた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link"
 import {SignInButton} from "@/components/auth/signin_button"
 import {SignOutButton} from "@/components/auth/signout_button"
-export default function Home() {
+import { auth } from "@/auth"
+
+export default async function Home() {
+  const session = await auth()
   return (
     <div>
       <h1>auto-saved-chat</h1>
       <Link href="/schedule">デモページ</Link>
-      <SignInButton />
-      <SignOutButton />
+      {session ? <SignOutButton /> : <SignInButton />}
     </div>
   )
 }
