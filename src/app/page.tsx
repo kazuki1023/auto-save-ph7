9a6b75fb53e9e40d1fbdb000067626f69596efeb
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-05-28 22:19:49 +0900

    add: supabase環境作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,21 @@ import Link from "next/link"
 import {SignInButton} from "@/components/auth/signin_button"
 import {SignOutButton} from "@/components/auth/signout_button"
 import { auth } from "@/auth"
+import { supabase } from "@/utils/supabase/supabaseClient"
 
 export default async function Home() {
   const session = await auth()
+
+  // App Router ではここで直接 fetch
+  const { data, error } = await supabase
+    .from('todos')
+    .select('*')
+
+  if (error) {
+    console.error(error)
+  }
+
+  console.log(data)
   return (
     <div>
       <h1>auto-saved-chat</h1>
