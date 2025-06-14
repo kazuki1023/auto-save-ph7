e99d2c6cc4594582ddff75753eaa6f47cbac69bd
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-06-14 12:30:06 +0900

    日程調整一覧ページ作成

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { Button } from '@heroui/button';
+import { Card, CardFooter, CardHeader } from '@heroui/card';
 import { Link } from '@heroui/link';
-import { Listbox, ListboxItem } from '@heroui/listbox';
 
 import { SignInButton } from '@/components/auth/signin_button';
-import { dummy_schedule } from '@/const/dummy_scedule';
+import { dummy_schedule } from '@/const/dummy_schedule';
 
 export default function Schedule() {
   return (
@@ -14,22 +14,24 @@ export default function Schedule() {
         <h1 className="text-2xl font-bold">ダミー日程調整</h1>
         <SignInButton />
       </div>
-      <div className="w-full max-w-[400px] px-1 py-2 rounded-small dark:border-default-100">
-        <Listbox className="flex flex-col gap-2 mb-2">
-          {dummy_schedule.map(schedule => (
-            <ListboxItem
-              key={schedule.id}
-              className="mb-2"
-              textValue={`${schedule.date} ${schedule.start_time} - ${schedule.end_time}`}
-            >
-              {schedule.date} {schedule.start_time} - {schedule.end_time}
-            </ListboxItem>
-          ))}
-        </Listbox>
+      <div className="w-full max-w-[400px] px-1 py-2 rounded-small dark:border-default-100 flex flex-col gap-2">
+        {dummy_schedule.map(schedule => (
+          <Card key={schedule.id}>
+            <CardHeader>
+              <h2 className="text-lg font-bold">{schedule.name}</h2>
+            </CardHeader>
+            <CardFooter className="flex justify-center">
+              <Button
+                color="primary"
+                as={Link}
+                href={`/schedule/register/${schedule.id}`}
+              >
+                登録する
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
-      <Button color="primary" as={Link} href="/schedule/register">
-        登録する
-      </Button>
     </>
   );
 }
