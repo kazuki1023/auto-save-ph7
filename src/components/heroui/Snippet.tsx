aa280923bd4183960f41a4c77da9320eb0ea9d15
kazuki1023 <k.keio12569@gmail.com>
2025-07-22 23:21:31 +0900

    components追加

---
+++ src/components/heroui/Snippet.tsx
@@ -0,0 +1,17 @@
+import { Snippet as HerouiSnippet, SnippetProps } from '@heroui/react';
+
+const Snippet = ({
+  children,
+  ...props
+}: {
+  children: React.ReactNode;
+  props: SnippetProps;
+}) => {
+  return (
+    <HerouiSnippet {...props} symbol="">
+      {children}
+    </HerouiSnippet>
+  );
+};
+
+export default Snippet;
