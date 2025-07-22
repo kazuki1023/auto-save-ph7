5706a9d1e39061c0fb638c7d5cf977ee58437ac8
kazuki1023 <k.keio12569@gmail.com>
2025-07-22 21:18:02 +0900

    不要ファイル削除

--- src/lib/supabase/questions/query.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from '../supabaseClient';
-
-export const getQuestions = async () => {
-  const { data, error } = await supabase.from('questions').select('*');
-  if (error) throw error;
-  return data;
-};
