318fce29a308e1204b30a713cd7f797ab4ebd729
kazuki1023 <k.keio12569@gmail.com>
2025-07-22 21:41:36 +0900

    fix

--- src/reposiroties/answers.ts
+++ src/reposiroties/answers.ts
@@ -1,6 +1,7 @@
+import { Tables } from '@/lib/supabase/database.types';
 import { supabase } from '@/lib/supabase/supabaseClient';
 
-export const getAnswers = async () => {
+export const getAnswers = async (): Promise<Tables<'answers'>[] | null> => {
   const { data, error } = await supabase.from('answers').select('*');
   if (error) throw error;
   return data;
--- src/reposiroties/questions.ts
+++ src/reposiroties/questions.ts
@@ -1,6 +1,7 @@
+import { Tables } from '@/lib/supabase/database.types';
 import { supabase } from '@/lib/supabase/supabaseClient';
 
-export const getQuestions = async () => {
+export const getQuestions = async (): Promise<Tables<'questions'>[] | null> => {
   const { data, error } = await supabase.from('questions').select('*');
   if (error) throw error;
   return data;
