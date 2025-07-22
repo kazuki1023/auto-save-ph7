c1a5d819055ef06ce62bd40b220408382230186f
kazuki1023 <k.keio12569@gmail.com>
2025-07-22 23:25:03 +0900

    add chip

---
+++ src/components/heroui/Chip.tsx
@@ -0,0 +1,7 @@
+import { Chip as HerouiChip, ChipProps } from '@heroui/react';
+
+const Chip = ({ children, ...props }: ChipProps) => {
+  return <HerouiChip {...props}>{children}</HerouiChip>;
+};
+
+export default Chip;
