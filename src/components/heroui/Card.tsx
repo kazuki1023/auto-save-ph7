b619483e8e832b42f670f15a7e74cb53c850d1ba
kazuki1023 <k.keio12569@gmail.com>
2025-08-04 12:07:47 +0900

    fix

--- src/components/heroui/Card.tsx
+++ src/components/heroui/Card.tsx
@@ -6,42 +6,46 @@ import {
   CardFooter as HeroUICardFooter,
   CardHeader as HeroUICardHeader,
   type CardProps as BaseCardProps,
+  type CardFooterProps as BaseCardFooterProps,
 } from '@heroui/react';
-import { forwardRef, type ReactNode } from 'react';
+import { forwardRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface CardProps extends BaseCardProps {}
-
-export interface CardBodyProps {
-  children?: ReactNode;
-  className?: string;
-}
-
-export interface CardHeaderProps {
-  children?: ReactNode;
-  className?: string;
-}
-
-export interface CardFooterProps {
-  children?: ReactNode;
-  className?: string;
-}
 
 const Card = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
   return <HeroUICard ref={ref} {...props} />;
 });
 
-const CardBody = forwardRef<HTMLDivElement, CardBodyProps>(({ children, ...props }, ref) => {
-  return <HeroUICardBody ref={ref} {...props}>{children}</HeroUICardBody>;
-});
+const CardBody = forwardRef<HTMLDivElement, CardProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <HeroUICardBody ref={ref} {...props}>
+        {children}
+      </HeroUICardBody>
+    );
+  }
+);
 
-const CardHeader = forwardRef<HTMLDivElement, CardHeaderProps>(({ children, ...props }, ref) => {
-  return <HeroUICardHeader ref={ref} {...props}>{children}</HeroUICardHeader>;
-});
+const CardHeader = forwardRef<HTMLDivElement, CardProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <HeroUICardHeader ref={ref} {...props}>
+        {children}
+      </HeroUICardHeader>
+    );
+  }
+);
 
-const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>(({ children, ...props }, ref) => {
-  return <HeroUICardFooter ref={ref} {...props}>{children}</HeroUICardFooter>;
-});
+const CardFooter = forwardRef<HTMLDivElement, BaseCardFooterProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <HeroUICardFooter ref={ref} {...props}>
+        {children}
+      </HeroUICardFooter>
+    );
+  }
+);
 
 Card.displayName = 'Card';
 CardBody.displayName = 'CardBody';
--- src/components/heroui/index.ts
+++ src/components/heroui/index.ts
@@ -1,14 +1,5 @@
 // HeroUI Components Wrapper
 export { default as Button, type ButtonProps } from './Button';
-export {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  type CardBodyProps,
-  type CardFooterProps,
-  type CardHeaderProps,
-  type CardProps,
-} from './Card';
+export { Card, CardBody, CardFooter, CardHeader, type CardProps } from './Card';
 export { default as Chip, type ChipProps } from './Chip';
 export { default as Link, type LinkProps } from './Link';
