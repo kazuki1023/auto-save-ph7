da7bedd12fab0d6bc9edf58a3e40a1ed36e6ad7e
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-08-04 15:45:29 +0900

    rm: 不要なコードを削除

--- src/app/answer/[uuid]/page.tsx
+++ src/app/answer/[uuid]/page.tsx
@@ -2,16 +2,39 @@
 // 回答画面
 
 import { Input } from '@heroui/input';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 import { Card, CardBody, CardFooter, CardHeader } from '@/components/heroui';
 import Button from '@/components/heroui/Button';
+import { supabase } from '@/lib/supabase/supabaseClient';
 
 // import { useParams } from 'next/navigation';
 
 const AnswerPage = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const { uuid } = useParams();
+  const { uuid } = useParams();
+
+  // データ取得用のstate
+  const [request, setRequest] = useState(null);
+
+  // uuidを元にrequestsテーブルからデータ取得
+  useEffect(() => {
+    if (!uuid) return;
+    const fetchRequest = async () => {
+      const { data, error } = await supabase
+        .from('requests')
+        .select('*')
+        .eq('id', uuid)
+        .single();
+      if (error) {
+        console.error(error);
+        return;
+      }
+      setRequest(data);
+    };
+    fetchRequest();
+  }, [uuid]);
 
   const router = useRouter();
 
@@ -20,23 +43,12 @@ const AnswerPage = () => {
     router.push('/answer/share');
   };
 
-  const data = {
-    plan: { days: 3, nights: 2 },
-    notes: '旅行の日程候補を作成しました',
-    candidates: [
-      { end: '2025-08-06T15:00:00.000Z', start: '2025-08-04T15:00:00.000Z' },
-      { end: '2025-08-12T15:00:00.000Z', start: '2025-08-10T15:00:00.000Z' },
-      { end: '2025-08-19T15:00:00.000Z', start: '2025-08-17T15:00:00.000Z' },
-      { end: '2025-08-26T15:00:00.000Z', start: '2025-08-24T15:00:00.000Z' },
-    ],
-  };
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
       <div className="mb-8 w-full max-w-lg">
         <div className="font-bold mb-4 text-xl text-center">候補日一覧</div>
         <ul className="flex flex-col gap-4">
-          {data.candidates.map((candidate, idx) => (
+          {request?.content_json?.candidates.map((candidate, idx) => (
             <Card
               className="max-w-[340px] border border-green-300 mx-auto"
               key={idx}
