dafb5a0e5b32ff7cbb337f1f8c62d6fda6887a71
kazuki1023 <k.keio12569@gmail.com>
2025-10-19 14:59:50 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   description:
     'E-Loveは、日程調整アプリです。Google Calendarと連携した自動入力やパターン毎に日程調整フォームなどを用意しており、いままで以上に快適な日程調整を提供します',
   verification: {
-    google: 'vU5QqsuaI1kaZnRmeAcNO81SClAbY4qPZrhaThoICVk',
+    google: process.env.GOOGLE_VERIFICATION_TOKEN,
   },
 };
 
